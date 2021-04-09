0db90dfdcb996da8b3dd94fb734abccb49bc5c0a
dilnuras <77263709+dilnuras@users.noreply.github.com>
2021-04-09 19:52:01 +0600

    добавили слайдер

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.channel-slider', {
+    // Optional parameters
+        loop: true,
+        slidesPerView:6,
+    navigation: {
+        nextEl: '.channel-button-next',
+        prevEl: '.channel-button-prev',
+      },
+    });
